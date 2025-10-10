b6124b210e24f76d4fc275f1e0ebf5c47ef6eb56
HongHac <144915081+chuonghoai@users.noreply.github.com>
2025-10-11 01:59:35 +0700

    Update mazePage.py

--- mazePage.py
+++ mazePage.py
@@ -344,7 +344,3 @@ class mazePage:
             row = line.strip().split()
             processed.append(row)
         return processed
-    
-root = tk.Tk()
-app = mazePage(root, "USAGI")
-root.mainloop()
