9ee46e8e9422f9a18abe180f628eef14a33bde00
HongHac <144915081+chuonghoai@users.noreply.github.com>
2025-10-14 05:59:50 +0700

    complete project

--- mazePage.py
+++ mazePage.py
@@ -230,9 +230,9 @@ class mazePage:
         
     # Vẽ combo box chọn thuật toán
     def draw_cbbox(self):
-        values = ["BFS", "Greedy", "A*", "IDL", "Simulated Annealing", "Forward Checking", 
-                  "And-Or Tree", "Belief state", "Partially observable", "AC-3", "DFS", "Hill Climbing",
-                  "UCS", "Beam Search", "Backtracking"]
+        values = ["BFS", "DFS","IDL", "UCS", "Greedy", "A*", "Hill Climbing", 
+                  "Simulated Annealing", "Beam Search", "And-Or Tree", "Belief state",
+                  "Partially observable", "Backtracking", "Forward Checking", "AC-3"]
         self.algorithmCbb = ComboBoxObj(self.canvas)
         self.algorithmCbb.createComboBox(
             self.width - 185, self.height//2 - 200,
